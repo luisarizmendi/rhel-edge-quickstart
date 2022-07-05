d1da95f9f0c733e2fac95e89bb21e61a9527b894
Luis Arizmendi <luis.ariz@gmail.com>
2022-07-05 11:18:39 +0200

    Demo FDO insights

--- demos/fdo-insights-cockpit/fdo-configs/device0/var/home/admin/change-hostname.sh
+++ demos/fdo-insights-cockpit/fdo-configs/device0/var/home/admin/change-hostname.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 hostnamectl set-hostname edge-virt-$(echo $RANDOM | md5sum | head -c 8; echo;)
 hostnamectl --pretty set-hostname edge-virt-$(echo $RANDOM | md5sum | head -c 8; echo;)
-
-cp /etc/hostname /mnt/sysroot/etc/hostname
-cp /etc/machine-info /mnt/sysroot/etc/machine-info
-
-
