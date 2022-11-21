d9ec4065b23d836bb9fea36c730eafc62a07f371
Luis Arizmendi <luis.ariz@gmail.com>
2022-11-21 09:12:14 +0100

    Demo microshift acm

--- demos/fdo-microshift-acm/prepare-fdo-server.sh
+++ demos/fdo-microshift-acm/prepare-fdo-server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+VAR_FILE=.vars
 
 firewall-cmd --add-port=8080-8083/tcp --permanent
 firewall-cmd --reload
 
 
+
+# This crazy stuff is because it didn't work with just removing the file and restarting the service...
 systemctl stop fdo-aio
 sleep 1
 rm -rf /etc/fdo/aio/*
@@ -25,23 +28,49 @@ systemctl restart fdo-aio
 
 
 
+if [ -f "$VAR_FILE" ]; then
+    RHORGID=$(grep RHORGID $VAR_FILE | awk -F RHORGID= '{print $2}')
+    ACTIVATIONKEY=$(grep ACTIVATIONKEY $VAR_FILE | awk -F ACTIVATIONKEY= '{print $2}')
+    SSHKEY=$(grep SSHKEY $VAR_FILE | awk -F SSHKEY= '{print $2}')
+else
+    echo ""
+
+    echo ""
+    echo "Enter your Red Hat Organization ID:"
+    read RHORGID
+
+    echo ""
+    echo "Enter your Red Hat Activation Key:"
+    read ACTIVATIONKEY
+
+    echo ""
+    echo "Enter your Public SSH Key:"
+    read SSHKEY
+fi
+
+
+
+
+
+
 sleep 5
 
 service_info_auth_token=$(grep service_info_auth_token /etc/fdo/aio/configs/serviceinfo_api_server.yml | awk '{print $2}')
 admin_auth_token=$(grep admin_auth_token /etc/fdo/aio/configs/serviceinfo_api_server.yml | awk '{print $2}')
 
-sed -i "/service_info_auth_token:*.*/d" serviceinfo_api_server.yml.example 
-sed -i "/admin_auth_token:*.*/d" serviceinfo_api_server.yml.example 
 
-echo "service_info_auth_token: $service_info_auth_token" >> serviceinfo_api_server.yml.example
-echo "admin_auth_token: $admin_auth_token" >> serviceinfo_api_server.yml.example
+yes | cp -f serviceinfo_api_server.yml.example serviceinfo_api_server.yml
 
+sed -i "s/<MY_ORGID>/${RHORGID}/g" serviceinfo_api_server.yml 
+sed -i "s/<MY_ACTIVATIONKEY>/${ACTIVATIONKEY}/g" serviceinfo_api_server.yml 
+sed -i "s|ssh-rsa AAAA|${SSHKEY}|g" serviceinfo_api_server.yml 
 
-
+sed -i "s|service_info_auth_token:*.*|service_info_auth_token: ${service_info_auth_token}|g" serviceinfo_api_server.yml.example 
+sed -i "s|admin_auth_token:*.*|admin_auth_token: ${admin_auth_token}|g" serviceinfo_api_server.yml.example 
 
 
 rm -rf  /etc/fdo/aio/configs/serviceinfo_api_server.yml
-cp -f serviceinfo_api_server.yml.example  /etc/fdo/aio/configs/serviceinfo_api_server.yml
+cp -f serviceinfo_api_server.yml  /etc/fdo/aio/configs/serviceinfo_api_server.yml
 
 rm -rf /etc/fdo-configs
 cp -r fdo-configs /etc/
